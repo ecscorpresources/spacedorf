cd93d1d47312f8a6418132da9fdf92cd26a246d4
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2020-02-07 08:58:42 +0100

    finished find an agent page

---
+++ src/components/includes/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Toolbar from "./Toolbar/Toolbar";
+import SideDrawer from "./SideDrawer/SideDrawer";
+import "./Navbar.css";
+import Backdrop from "./Backdrop/Backdrop";
+import { withRouter } from "react-router-dom";
+
+class Navbar extends Component {
+  state = {
+    SideDrawerOpen: false
+  };
+  handleDrawerToggleClick = () => {
+    this.setState(prevState => {
+      return {
+        SideDrawerOpen: !prevState.SideDrawerOpen
+      };
+    });
+  };
+  handleBackdropClick = () => {
+    this.setState({
+      SideDrawerOpen: false
+    });
+  };
+
+  render() {
+    console.log(this.props);
+    let myBackdrop;
+    if (this.state.SideDrawerOpen) {
+      myBackdrop = <Backdrop handleBackdropClick={this.handleBackdropClick} />;
+    }
+    return (
+      <div style={{ height: "100%" }}>
+        <Toolbar handleDrawerToggleClick={this.handleDrawerToggleClick} />
+        <SideDrawer show={this.state.SideDrawerOpen} />
+        {myBackdrop}
+        {/* <main style={{ marginTop: "56px" }}></main> */}
+      </div>
+    );
+  }
+}
+
+export default withRouter(Navbar);
