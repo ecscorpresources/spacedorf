ba827de0f25f1e22e1190a62229adb6bfd4f40a8
shina <rasheedshinaopeyemi@gmail.com>
2020-08-26 12:24:45 +0100

    added the agent property info to the dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,6 +8,7 @@ import smallhero from "../../assets/img/11.png";
 //import the agents and get a random agent for now
 import agents from "../../data/agents";
 import Spinner from "../Globals/Spinner";
+import styled from "@emotion/styled";
 
 class Dashboard extends Component {
   state = {
@@ -21,6 +22,7 @@ class Dashboard extends Component {
     const id = 3;
 
     const agent = this.state.agents.find((agent) => agent.id === id);
+    console.log(agent);
 
     return (
       <>
@@ -30,66 +32,84 @@ class Dashboard extends Component {
           <Spinner />
         ) : (
           <>
-            <section>
-              <div>
+            <DashboardWrapper>
+              <div className="mb-5">
                 {/* can include agent's name */}
                 <h1>Agent Dashboard</h1>
-                <article>
-                  <div>
-                    <div>
-                      <Tabs variant="unstyled">
-                        <TabList>
-                          <Tab
-                            pl={0}
-                            bg="transparent"
-                            border="0"
-                            _focus={{ outline: "none" }}
-                          >
-                            My Properties
-                          </Tab>
-                          <Tab
-                            pl={0}
-                            bg="transparent"
-                            border="0"
-                            _focus={{ outline: "none" }}
-                          >
-                            Profile
-                          </Tab>
-
-                          <Tab
-                            pl={0}
-                            bg="transparent"
-                            border="0"
-                            _focus={{ outline: "none" }}
-                          >
-                            Settings
-                          </Tab>
-                        </TabList>
-
-                        <TabPanels>
-                          <TabPanel>
-                            <Properties />
-                          </TabPanel>
-
-                          <TabPanel>
-                            <Profile />
-                          </TabPanel>
-
-                          <TabPanel>
-                            <Settings />
-                          </TabPanel>
-                        </TabPanels>
-                      </Tabs>
-                    </div>
-                  </div>
-                </article>
               </div>
-            </section>
+              <article className="d-flex">
+                <div className="mr-5">
+                  <img
+                    style={{ width: "160px" }}
+                    src={agent.img}
+                    alt="agent banner"
+                  />
+                </div>
+                <div className="flex-grow-1">
+                  <Tabs variant="unstyled">
+                    <TabList
+                      bg="#f7f7f7"
+                      style={{ padding: "1rem 0 1rem 15rem" }}
+                    >
+                      <Tab
+                        mr={24}
+                        pl={0}
+                        bg="transparent"
+                        border="0"
+                        _focus={{ outline: "none" }}
+                        style={{ fontSize: "1.7rem" }}
+                      >
+                        My Properties
+                      </Tab>
+                      <Tab
+                        mr={24}
+                        pl={0}
+                        bg="transparent"
+                        border="0"
+                        _focus={{ outline: "none" }}
+                        style={{ fontSize: "1.7rem" }}
+                      >
+                        Profile
+                      </Tab>
+
+                      <Tab
+                        pl={0}
+                        bg="transparent"
+                        border="0"
+                        _focus={{ outline: "none" }}
+                        style={{ fontSize: "1.7rem" }}
+                      >
+                        Settings
+                      </Tab>
+                    </TabList>
+
+                    <TabPanels>
+                      <TabPanel>
+                        <Properties agent={agent} />
+                      </TabPanel>
+
+                      <TabPanel>
+                        <Profile />
+                      </TabPanel>
+
+                      <TabPanel>
+                        <Settings />
+                      </TabPanel>
+                    </TabPanels>
+                  </Tabs>
+                </div>
+              </article>
+            </DashboardWrapper>
           </>
         )}
       </>
     );
   }
 }
+
+const DashboardWrapper = styled.section`
+  width: 75vw;
+  margin: 4rem auto;
+`;
 
 export default Dashboard;
--- src/components/Dashboard/Properties.js
+++ src/components/Dashboard/Properties.js
@@ -1,7 +1,33 @@
 import React from "react";
 
-const Properties = () => {
-  return <div>Properties</div>;
+const Properties = ({ agent }) => {
+  return (
+    <>
+      <table>
+        <thead className="text-capitalize">
+          <tr>
+            <th>PropertyTYpe</th>
+            <th>Location</th>
+            <th>Sale/Rent</th>
+            <th>Price</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Properties;
